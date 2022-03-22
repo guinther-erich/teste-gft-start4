6a160951bca5f599f50435a17f8b39d1ee971cdb
guinther-erich <guinther.erich@gmail.com>
2022-03-22 16:57:22 -0300

    second question

---
+++ src/gft/start/guinthererich/question/two/Pessoa.java
@@ -0,0 +1,41 @@
+package gft.start.guinthererich.question.two;
+
+public class Pessoa {
+    protected String nome;
+    protected String endereco;
+    protected String telefone;
+
+    public Pessoa() {
+        super();
+    }
+
+    public void imprimirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Telefone: " + telefone);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
